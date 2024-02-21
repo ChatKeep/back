365ecbdd241e130896a37d897adc0872d4799ac7
dawoon jung <daewangbali@gmail.com>
2024-02-22 01:50:25 +0900

    Initial commit

---
+++ src/main/java/com/project/chatkeep/ChatkeepApplication.java
@@ -0,0 +1,13 @@
+package com.project.chatkeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatkeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatkeepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/chatkeep/ChatkeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.chatkeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatkeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
